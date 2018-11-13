154057525ffc39135797bda7f63e2c171595ca19
Huhspaniel <41580234+Huhspaniel@users.noreply.github.com>
2018-11-13 10:16:20 -0500

    Refactor api-routes

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -1,27 +1,43 @@
 const db = require('../models');
 const bcrypt = require('bcrypt');
 
+const authenticate = function (req, res, next) {
+    if (req.header('AdminKey') === process.env.API_KEY) {
+        next();
+    } else {
+        res.status(401).send('401 Unauthorized');
+    }
+}
+const validateUser = function (req, res, next) {
+    console.log('User validated!')
+    next();
+}
+const encryptPassword = function (req, res, next) {
+    bcrypt.hash(req.body.username + req.body.password, 10, (err, hash) => {
+        req.body.password = hash;
+        next();
+    })
+}
+
 module.exports = function (app) {
-    app.get('/api/users', (req, res) => {
-        if (req.header('AdminKey') === process.env.API_KEY) {
-            db.User.findAll()
-                .then(data => res.json(data))
-                .catch(err => res.json(err));
-        } else {
-            res.status(401).send('401 Unauthorized');
-        }
+    app.get('/api/users', authenticate, (req, res) => {
+        db.User.findAll()
+            .then(data => res.json(data))
+            .catch(err => res.json(err));
     });
     app.get('/api/users/:username', (req, res) => {
         db.User.findOne({ where: { username: req.params.username } })
             .then(data => res.json(data))
             .catch(err => res.json({ error: err }));
     })
-    app.post('/api/users', (req, res) => {
-        bcrypt.hash(req.body.username + req.body.password, 10, (err, hash) => {
-            req.body.password = hash;
-            db.User.create(req.body)
-                .then(data => res.json(data))
-                .catch(err => res.json({ error: err }));
-        })
+    app.post('/api/users', encryptPassword, (req, res) => {
+        db.User.create(req.body)
+            .then(data => res.json(data))
+            .catch(err => res.json({ error: err }));
+    });
+    app.put('/api/users/:id', validateUser, (req, res) => {
+        db.User.update(req.body, { where: { id: req.params.id } })
+            .then(data => res.json(data))
+            .catch(err => res.json({ error: err }));
     });
 }
