449438e91249d14778342fe75ff20f8ce2661865
afw821 <afw821@gmail.com>
2018-11-13 10:48:37 -0500

    I added styling and content to the user interface

--- app/config/config.js
+++ app/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     development: {
         username: "root",
-        password: process.env.DB_PASS,
+        password: "password",
         database: "db",
         host: "127.0.0.1",
         port: 3306,
