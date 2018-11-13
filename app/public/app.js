6e1240df61785cd917faed2c0ca20adf64e10414
afw821 <afw821@gmail.com>
2018-11-13 12:36:33 -0500

    I made changes to the nav links so they will navigate user to the corresponding page

--- app/public/app.js
+++ app/public/app.js
@@ -1,5 +1,5 @@
 
-
+//this function hides the welcome page and shows the search page on button click
 const hideHomePage = function(){
     $('.welcome-page').addClass('hide');
     $('.band-page').removeClass('hide');
@@ -7,3 +7,10 @@ const hideHomePage = function(){
 $('.login-button').on('click', hideHomePage);
 $('.search-artists').on('click', hideHomePage);
 
+//this function allows the user to go back to the home page when home is clicked on navbar
+
+const goHome = function(){
+    $('.welcome-page').removeClass('hide');
+    $('.band-page').addClass('hide');
+}
+$('.go-home').on('click', goHome);
