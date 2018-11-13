66744f6b433637f318a9280be980f34676437565
Huhspaniel <41580234+Huhspaniel@users.noreply.github.com>
2018-11-13 11:14:20 -0500

    Refactor api routes to use class structure

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -1,5 +1,6 @@
 const db = require('../models');
 const bcrypt = require('bcrypt');
+const RestfulAPI = require('./RestfulAPI');
 
 const authenticate = function (req, res, next) {
     if (req.header('AdminKey') === process.env.API_KEY) {
@@ -13,31 +14,19 @@ const validateUser = function (req, res, next) {
     next();
 }
 const encryptPassword = function (req, res, next) {
-    bcrypt.hash(req.body.username + req.body.password, 10, (err, hash) => {
-        req.body.password = hash;
-        next();
-    })
+    if (req.body.password) {
+        bcrypt.hash(req.body.username + req.body.password, 10, (err, hash) => {
+            req.body.password = hash;
+            next();
+        });
+    }
 }
 
 module.exports = function (app) {
-    app.get('/api/users', authenticate, (req, res) => {
-        db.user.findAll()
-            .then(data => res.json(data))
-            .catch(err => res.json(err));
-    });
-    app.get('/api/users/:username', (req, res) => {
-        db.user.findOne({ where: { username: req.params.username } })
-            .then(data => res.json(data))
-            .catch(err => res.json({ error: err }));
-    })
-    app.post('/api/users', encryptPassword, (req, res) => {
-        db.user.create(req.body)
-            .then(data => res.json(data))
-            .catch(err => res.json({ error: err }));
-    });
-    app.put('/api/users/:id', validateUser, (req, res) => {
-        db.user.update(req.body, { where: { id: req.params.id } })
-            .then(data => res.json(data))
-            .catch(err => res.json({ error: err }));
-    });
+    const user = new RestfulAPI('users', db.user, app);
+    user.findAll(null, null, authenticate);
+    user.findOne('username', null);
+    user.create(encryptPassword);
+    user.update('username', validateUser, encryptPassword);
+    user.delete('username', validateUser);
 }
