d2d136ccc73749e29a283bdda8a5b29a08072577
David Blais <davidblais@Jessica-Knippel-MacBook-Pro.local>
2018-11-14 12:38:49 -0500

    Added socket.io functionality. It's currently having issues populating messages at current moment. It's being populated, but it's not showing up on the page. I think it has something to do with the UI.

--- app/public/app.js
+++ app/public/app.js
@@ -1,4 +1,3 @@
-
 //this function hides the welcome page and shows the search page on button click
 const hideHomePage = function(){
     $('.welcome-page').addClass('hide');
@@ -7,10 +6,30 @@ const hideHomePage = function(){
 $('.login-button').on('click', hideHomePage);
 $('.search-artists').on('click', hideHomePage);
 
+// This is the setup for Socket.io
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 8080;
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
+
 //this function allows the user to go back to the home page when home is clicked on navbar
 
 const goHome = function(){
-    $('.welcome-page').removeClass('hide');
-    $('.band-page').addClass('hide');
+    $('.welcome-page').addClass('hide');
+    $('.band-page').removeClass('hide');
 }
 $('.go-home').on('click', goHome);
