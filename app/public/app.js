0d66b52589cec5b9048abb5f7f42d73f420dcb39
Huhspaniel <41580234+Huhspaniel@users.noreply.github.com>
2018-11-14 12:44:40 -0500

    Fix bugs and refactor
    Add spotify login page

--- app/public/app.js
+++ app/public/app.js
@@ -1,21 +1,3 @@
-
-// //this function hides the welcome page and shows the search page on button click
-// const hideHomePage = function () {
-//     $('.welcome-page').addClass('hide');
-//     $('.band-page').removeClass('hide');
-// }
-// $('.login-button').on('click', hideHomePage);
-// $('.search-artists').on('click', hideHomePage);
-
-// //this function allows the user to go back to the home page when home is clicked on navbar
-
-// const goHome = function () {
-//     $('.welcome-page').removeClass('hide');
-//     $('.band-page').addClass('hide');
-// }
-// $('.go-home').on('click', goHome);
-
-// const mains = document.getElementsByTagName('main');
 const xhr = new XMLHttpRequest();
 
 const usernameLogin = document.querySelector('.login-box .username');
@@ -25,24 +7,34 @@ const usernameCreate = document.querySelector('.create-box .username');
 const passwordCreate = document.querySelector('.create-box .password');
 let token;
 
+const clearInputs = (e) => {
+    e.preventDefault();
+    Array.from(document.querySelectorAll('.login-box input, .create-box input')).forEach(el => {
+        el.value = '';
+    });
+}
+
 const loadPage = e => {
     if (e.target.tagName === 'BUTTON') {
         document.querySelector('main:not(.hidden)').classList.add('hidden');
         document.querySelector(`main.${e.target.classList[1]}`).classList.remove('hidden');
     }
 }
-
-document.querySelector('nav').addEventListener('click', loadPage);
-document.querySelector('.nav.create-account').addEventListener('click', loadPage);
+Array.from(document.getElementsByClassName('nav')).forEach(el => {
+    el.addEventListener('click', loadPage);
+})
 
 document.querySelector('.submit-login').addEventListener('click', e => {
     e.preventDefault();
     xhr.open('POST', '/login');
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.onload = () => {
-        const res = xhr.response;
-        if (res.auth) token = res.token;
-        console.log(JSON.parse(auth));
+        const res = JSON.parse(xhr.response);
+        if (res.auth) {
+            token = res.token;
+            clearInputs(e);
+        }
+        console.log(res);
     }
     xhr.send(JSON.stringify({
         username: usernameLogin.value,
@@ -55,27 +47,13 @@ document.querySelector('.submit-create').addEventListener('click', e => {
     xhr.open('POST', '/api/users');
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.onload = () => {
-        const user = xhr.response;
-        console.log(JSON.parse(user));
+        const user = JSON.parse(xhr.response);
+        if (!user.error) clearInputs(e);
+        console.log(user);
     }
     xhr.send(JSON.stringify({
         username: usernameCreate.value,
         password: passwordCreate.value,
         email: emailCreate.value
     }))
-})
-
-// $(function () {
-
-// let queryURL = `https://api.spotify.com/v1/search?q=${bandName}&type=artist&market=US&limit=10&offset=5`
-
-//     $.ajax({
-//         url: queryURL,
-//         method: 'GET',
-//         headers: {
-
-//         }
-//     }).then(function(response){
-//         console.log(response);
-//     })
-// })
+});
