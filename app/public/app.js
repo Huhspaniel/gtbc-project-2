d7b1684473f326f0b5b816a6c0b90293c10dd83f
afw821 <afw821@gmail.com>
2018-11-13 22:09:28 -0500

    I added information to the ajax call on app.js

--- app/public/app.js
+++ app/public/app.js
@@ -1,6 +1,6 @@
 
 //this function hides the welcome page and shows the search page on button click
-const hideHomePage = function(){
+const hideHomePage = function () {
     $('.welcome-page').addClass('hide');
     $('.band-page').removeClass('hide');
 }
@@ -9,8 +9,23 @@ $('.search-artists').on('click', hideHomePage);
 
 //this function allows the user to go back to the home page when home is clicked on navbar
 
-const goHome = function(){
+const goHome = function () {
     $('.welcome-page').removeClass('hide');
     $('.band-page').addClass('hide');
 }
 $('.go-home').on('click', goHome);
+
+$(function () {
+
+let queryURL = `https://api.spotify.com/v1/search?q=${bandName}&type=artist&market=US&limit=10&offset=5`
+
+    $.ajax({
+        url: queryURL,
+        method: 'GET',
+        headers: {
+
+        }
+    }).then(function(response){
+        console.log(response);
+    })
+})
