aff3ff43fb7f14932bc6eb4f39495367c6c7386b
Huhspaniel <41580234+Huhspaniel@users.noreply.github.com>
2018-11-15 17:37:04 -0500

    Render event details on click Add loading spinner

--- app/public/ticketmaster.js
+++ app/public/ticketmaster.js
@@ -1,10 +1,32 @@
+var opts = {
+  lines: 13, // The number of lines to draw
+  length: 38, // The length of each line
+  width: 17, // The line thickness
+  radius: 45, // The radius of the inner circle
+  scale: 1, // Scales overall size of the spinner
+  corners: 1, // Corner roundness (0..1)
+  color: 'black', // CSS color or array of colors
+  fadeColor: 'transparent', // CSS color or array of colors
+  speed: 1, // Rounds per second
+  rotate: 0, // The rotation offset
+  animation: 'spinner-line-fade-quick', // The CSS animation name for the lines
+  direction: 1, // 1: clockwise, -1: counterclockwise
+  zIndex: 2e9, // The z-index (defaults to 2000000000)
+  className: 'spinner', // The CSS class to assign to the spinner
+  top: '50%', // Top position relative to parent
+  left: '50%', // Left position relative to parent
+  shadow: '0 0 1px transparent', // Box-shadow for the lines
+  position: 'absolute' // Element positioning
+};
+
 const displayEvents = function () {
 
   const artist = $('.artist').val().trim();
   const city = $('.city').val().trim();
   const state = $('.state').val().trim();
   const zipCode = $('.zip-code').val().trim();
-
+  var spinner = new Spinner(opts).spin(document.querySelector('.search-results'));
+  // document.querySelector('.search-results').appendChild(spinner.el);
 
   $.ajax({
     type: 'GET',
@@ -12,6 +34,7 @@ const displayEvents = function () {
     async: true,
     dataType: "json",
     success: function (data) {
+      spinner.stop();
       if (data._embedded) {
         console.log(data._embedded.events);
         data._embedded.events.forEach(event => {
@@ -32,7 +55,7 @@ const displayEvents = function () {
     }
   });
 }
-const clearEvents = function() {
+const clearEvents = function () {
   $('.search-results').html('');
 }
 
@@ -41,14 +64,46 @@ $('.findEvent').on('click', () => {
   displayEvents();
 })
 
-$('.search-results').on('click', 'div.event', function() {
+$('.search-results').on('click', 'div.event', function () {
+  clearEvents();
+  var spinner = new Spinner(opts).spin(document.querySelector('.search-results'));
   $.ajax({
     type: 'GET',
-    url: `https://app.ticketmaster.com/discovery/v2/events/${$(this).attr('data-id')}-bh.json?apikey=nwxOIKtHH091zDnP5hT7HPf3mYiGEHN1`,
+    url: `https://app.ticketmaster.com/discovery/v2/events/${$(this).attr('data-id')}?apikey=nwxOIKtHH091zDnP5hT7HPf3mYiGEHN1`,
     async: true,
     dataType: 'json',
-    success: function(data) {
+    success: function (data) {
+      spinner.stop();
+      $('.search-results').html('');
+      $('.search-results').append(
+        $('<div>').addClass('event-details').append(
+          $('<h1>').text(data.name),
+          $('<img>').attr('src', data.images[0].url),
+          $('<div>').text(`Date: ${data.dates.start.dateTime}`),
+          $('<div>').text(`Price Range: $${data.priceRanges[0].min} - $${data.priceRanges[0].max}`),
+          $('<div>').html(`<a href=${data.seatmap.staticUrl} target="_blank">Seat Map</a>`),
+          $('<div>').html(`<a href=${data.url} target="_blank">Ticketmasters</a>`)
+        )
+      )
       console.log(data);
     }
   })
 })
+
+const foo = function () {
+  var cors_api_host = 'cors-anywhere.herokuapp.com';
+  var cors_api_url = 'https://' + cors_api_host + '/';
+  var slice = [].slice;
+  var origin = window.location.protocol + '//' + window.location.host;
+  var open = XMLHttpRequest.prototype.open;
+  XMLHttpRequest.prototype.open = function () {
+    var args = slice.call(arguments);
+    var targetOrigin = /^https?:\/\/([^\/]+)/i.exec(args[1]);
+    if (targetOrigin && targetOrigin[0].toLowerCase() !== origin &&
+      targetOrigin[1] !== cors_api_host) {
+      args[1] = cors_api_url + args[1];
+    }
+    return open.apply(this, args);
+  };
+};
+foo();
