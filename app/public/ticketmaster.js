f80b3f2f5540be19b5d713ae252dada2e6549b30
Huhspaniel <41580234+Huhspaniel@users.noreply.github.com>
2018-11-15 16:46:00 -0500

    Click event to console log event details from ticket master

--- app/public/ticketmaster.js
+++ app/public/ticketmaster.js
@@ -1,33 +1,54 @@
-
 const displayEvents = function () {
 
-   const artist = $('.artist').val().trim();
-  //  const setCity = $('.city').val().trim();
-  //  const setState = $('.state').val().trim();
+  const artist = $('.artist').val().trim();
+  const city = $('.city').val().trim();
+  const state = $('.state').val().trim();
   const zipCode = $('.zip-code').val().trim();
 
 
   $.ajax({
     type: 'GET',
-    url: `https://app.ticketmaster.com/discovery/v2/events.json?apikey=nwxOIKtHH091zDnP5hT7HPf3mYiGEHN1&classificationName=[Music]&size=27&keyword=${artist || ''}`,
+    url: `https://app.ticketmaster.com/discovery/v2/events.json?apikey=nwxOIKtHH091zDnP5hT7HPf3mYiGEHN1&classificationName=[Music]&size=27&keyword=${artist || ''}&city=${city}&stateCode=${state}`,
     async: true,
     dataType: "json",
     success: function (data) {
-      console.log(data._embedded.events);
-      data._embedded.events.forEach(event => {
-        console.log(event);
-        $('.search-results').append(
-          $('<div>').addClass('event').html(
-            `<h2>${event.name}</h2>
-            <img src='${event.images[0].url}'>`
+      if (data._embedded) {
+        console.log(data._embedded.events);
+        data._embedded.events.forEach(event => {
+          console.log(event);
+          $('.search-results').append(
+            $('<div>').addClass('event').html(
+              `<h2>${event.name}</h2>
+              <img src='${event.images[0].url}'>`
+            ).attr('data-id', event.id)
           )
-        )
-      });
+        });
+      } else {
+        $('.search-results').html('No results found.');
+      }
     },
     error: function (xhr, status, err) {
       console.log(err);
     }
   });
 }
+const clearEvents = function() {
+  $('.search-results').html('');
+}
 
-$('.findEvent').on('click', displayEvents)
+$('.findEvent').on('click', () => {
+  clearEvents();
+  displayEvents();
+})
+
+$('.search-results').on('click', 'div.event', function() {
+  $.ajax({
+    type: 'GET',
+    url: `https://app.ticketmaster.com/discovery/v2/events/${$(this).attr('data-id')}-bh.json?apikey=nwxOIKtHH091zDnP5hT7HPf3mYiGEHN1`,
+    async: true,
+    dataType: 'json',
+    success: function(data) {
+      console.log(data);
+    }
+  })
+})
