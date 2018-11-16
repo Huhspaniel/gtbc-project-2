d7fb2ccaa202925e4bd1a47c4c05eb35b061268c
Huhspaniel <41580234+Huhspaniel@users.noreply.github.com>
2018-11-16 02:14:31 -0500

    Fix username display bug

--- app/public/socket.js
+++ app/public/socket.js
@@ -1,12 +1,12 @@
 $(function () {
   var socket = io();
   $('form').submit(function () {
-    socket.emit('chat message', $('#m').val());
+    socket.emit('chat message', `<span style="color:orange">@${localStorage.getItem('username') || 'Anonymous'}:</span> ${$('#m').val()}`);
     $('#m').val('');
     return false;
   });
   socket.on('chat message', function (msg) {
-    $('#socketMessages').append($('<li>').html(`<span style="color:orange">@${localStorage.getItem('username') || 'Anonymous'}:</span> ${msg}`));
+    $('#socketMessages').append($('<li>').html(msg));
     const chatBox = document.querySelector('#socketMessages');
     chatBox.scrollTop = chatBox.scrollHeight;
   });
